5ab492ea3a266bbae032830a75afe47f4220f747
Eren AVAR <eren.avar@hotmail.com>
2024-10-24 22:39:17 +0300

    add uncompleted dividing form

--- src/components/DivideForm.js
+++ src/components/DivideForm.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Button from "./Button";
 
 export default function DivideForm() {
   return (
     <form className="form-split-bill">
       <h2>Divide an Expense with X</h2>
+      <label>💰 Total Value</label>
+      <input type="text" />
+
+      <label>👀 Ypur Expense</label>
+      <input type="text" />
+
+      <Button>Divide Expense</Button>
     </form>
   );
 }
